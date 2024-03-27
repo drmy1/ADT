c897d9aa0989ea1df126603e4ee9fb25534a0261
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-03-27 13:26:05 +0100

    CV_7_Sync_01

--- CV-07_(27_03_24)/main.py
+++ CV-07_(27_03_24)/main.py
@@ -3,9 +3,10 @@ import numpy as np
 
 EMPTY_VALUE = 0
 
+
 class SudokuSolver:
 
-    def __init__(self, field:np.ndarray|None=None, n_rows:int=9, n_columns:int=9):
+    def __init__(self, field: np.ndarray | None = None, n_rows: int = 9, n_columns: int = 9):
 
         if field is None:
             field = np.zeros([n_rows, n_columns], dtype=int)
@@ -13,40 +14,43 @@ class SudokuSolver:
         self.n_rows = n_rows
         self.n_columns = n_columns
 
-
-    def check_sequence(self, sequence:np.ndarray):
+    def check_sequence(self, sequence: np.ndarray):
         return True
-        
 
-    def check_one_cell(self, row_index:int , column_index:int):
+    def check_one_cell(self, row_index: int, column_index: int):
         return False
-        
 
-    def check_row(self, row_index:int):
+    def check_row(self, row_index: int):
         pass
 
-    def check_column(self, column_index:int):
+    def check_column(self, column_index: int):
         pass
 
-    def check_block(self, row_index:int, column_index:int):
+    def check_block(self, row_index: int, column_index: int):
         pass
 
-    def solve(self): 
-        
-        return False 
+    def solve(self):
+
+        return False
 
     def check_field(self):
         return True
-       
+
+    def load(self, path):
+        with open(path, "r") as file:
+            lines = file.readlines()
+            numeric_line = [[int(v) for v in line.strip().split(";")]
+                            for line in lines]
+            self.field = np.array(numeric_line)
 
 
-def main():    
+def main():
     sudoku_solver = SudokuSolver()
-    # sudoku_solver.load("sudoku.csv")
-    
+    sudoku_solver.load("data/removed_10.csv")
+
     print(sudoku_solver.solve())
     print(sudoku_solver.field)
-    
+
+
 if __name__ == "__main__":
     main()
-
