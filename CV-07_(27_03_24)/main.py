dc7014fc50a818f1cf6b6b815ddec2dfed518caf
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-04-03 14:52:09 +0200

    Sync

--- CV-07_(27_03_24)/main.py
+++ CV-07_(27_03_24)/main.py
@@ -15,25 +15,79 @@ class SudokuSolver:
         self.n_columns = n_columns
 
     def check_sequence(self, sequence: np.ndarray):
-        return True
+
+        arr = sequence
+
+        return len(np.unique(arr[arr != 0])) == len(arr[arr != 0])
 
     def check_one_cell(self, row_index: int, column_index: int):
-        return False
+
+        return self.check_row(row_index) and \
+            self.check_column(column_index) and \
+            self.check_block(row_index, column_index)
 
     def check_row(self, row_index: int):
-        pass
+
+        return self.check_sequence(self.field[row_index, :])
 
     def check_column(self, column_index: int):
-        pass
+
+        return self.check_sequence(self.field[:, column_index])
 
     def check_block(self, row_index: int, column_index: int):
-        pass
+        return self.check_sequence(self.field[row_index - row_index % 3:row_index - row_index % 3 + 3,
+                                              column_index - column_index % 3:column_index - column_index % 3 + 3])
 
     def solve(self):
+        for r in range(self.n_rows):
+            for c in range(self.n_columns):
+                # skip filled cells
+                if self.field[r, c] != EMPTY_VALUE:
+                    continue
 
-        return False
+                # fill empty cell with some value
+                possible_values = list(range(1, 10))
+                random.shuffle(possible_values)
+                for value in possible_values:
+                    self.field[r, c] = value
+
+                    # check if the number fits the cell
+                    if not self.check_one_cell(r, c):
+                        continue
+
+                    # try to solve another cell (recursion)
+                    if self.solve():
+                        return True
+
+                # no number fits the cell (the `return True` statement was not reached) -> backtrack
+                self.field[r, c] = EMPTY_VALUE
+                return False
+
+        # all cells are filled -- the sudoku is solved!
+        return True
+
+    """        
+    if self.check_sequence(self.field[:, :]):
+            return True
+        else:
+            for fields in self.field:
+                for i in range(9):
+                    if fields[i] == EMPTY_VALUE:
+                        for j in range(1, 10):
+                            if self.check_one_cell(i, j):
+                                fields[i] = j
+                                if self.solve():
+                                    return True
+                                else:
+                                    fields[i] = EMPTY_VALUE
+                        return False
+            return True"""
 
     def check_field(self):
+        for i in range(9):
+            for j in range(9):
+                if not self.check_one_cell(i, j):
+                    return False
         return True
 
     def load(self, path):
--- DU_02/main.py
+++ DU_02/main.py
@@ -1,12 +1,11 @@
-
 import seaborn as sns
 from typing import List, Tuple
 
 
 # vstupem budou úseky ze zadání [(20,.5),(1,-5),(10,2)]
-def simulate(accelerations: List[Tuple[int, int]]):
-    distance = 0  # počáteční vzdálenost
-    speed = 0  # počáteční rychlost
+def simulate(accelerations: List[Tuple[int, float]]):
+    distance = 0.0  # počáteční vzdálenost
+    speed = 0.0  # počáteční rychlost
 
     speeds = list()
     distances = list()
@@ -15,12 +14,13 @@ def simulate(accelerations: List[Tuple[int, int]]):
             # uložíme si aktuální hodnoty pro vizualizaci (okamžitá rychlost (speeds) a vzdálenost od počátku (distances))
 
             # TODO vypočítejte aktuální rychlost, tak, že na aktuální hodnotu rychlosti aplikujete zrychlení
-            speeds = speed + (accelerate * second) / abs(accelerate)
-
+            speed += accelerate
             # TODO vypočítejte přírůstek vzdálenosti za jednotku času a přidejte jej do již uražené vzdálenosti
-            distances = speed * duration
+            distance += speed
 
             # TODO uložíme si aktuální hodnoty pro vizualizaci (okamžitá rychlost a vzdálenost od počátku)
+            speeds.append(speed)
+            distances.append(distance)
 
             # vypíšeme si aktuální hodnoty
             print(f"{second=} {speed=} {distance=}")
@@ -34,6 +34,3 @@ speeds, distances = simulate(accelerations)
 
 # x roste od 0 do počtu prvků v seznamu, y je seznam rychlostí
 sns.lineplot(x=range(len(speeds)), y=(speeds))
-
-# x roste od 0 do počtu prvků v seznamu, y je seznam vzdáleností
-sns.lineplot(x=range(len(speeds)), y=(distances))
